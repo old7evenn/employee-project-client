53e8b25a66ef4734eb85c51e5444b7e85d4979aa
old7evenn <stepanukroman9@gmail.com>
2024-03-17 17:53:48 +0200

    change url server

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,8 +1,9 @@
 import {createApi, fetchBaseQuery, retry} from '@reduxjs/toolkit/query/react'
 import { RootState } from '../store'
 
+const PORT = 'https://employee-project-server-production.up.railway.app/api'
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:5000/api',
+  baseUrl: PORT,
   prepareHeaders: (headers, {getState}) => {
     const token = (getState() as RootState).auth.user?.token || localStorage.getItem('token')
 
